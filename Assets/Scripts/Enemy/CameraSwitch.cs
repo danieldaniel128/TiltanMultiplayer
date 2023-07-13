cb47f4f142e3b93f2e8dbd16d0f2b153d6393a55
amit800IL <108745954+amit800IL@users.noreply.github.com>
2023-07-13 22:02:36 +0300

    initial Camera Switch- not yet working

--- Assets/Scripts/Enemy/CameraSwitch.cs
+++ Assets/Scripts/Enemy/CameraSwitch.cs
@@ -1,7 +1,57 @@
 
+using Photon.Pun;
 using UnityEngine;
 
-public class CameraSwitch : MonoBehaviour
+public class CameraSwitch : MonoBehaviourPun
 {
-   
+    [SerializeField] private GameObject[] Cameras;
+    private GameObject mainCamera;
+    private void Start()
+    {
+        mainCamera = Cameras[0];
+        mainCamera.SetActive(true);
+
+        for (int i = 0; i < Cameras.Length; i++)
+        {
+            if (Cameras[i] != Cameras[0])
+            {
+                Cameras[i].SetActive(false);
+            }
+        }
+    }
+    private void Update()
+    {
+        ChangeCamera();
+    }
+    private void ChangeCamera()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            for (int i = 0; i < Cameras.Length; i++)
+            {
+                GameObject currentCamera = Cameras[i--];
+
+                currentCamera.SetActive(false);
+
+                GameObject nextCamera = Cameras[i];
+
+                nextCamera.SetActive(true);
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            for (int i = 0; i < Cameras.Length; i++)
+            {
+                GameObject currentCamera = Cameras[i--];
+
+                currentCamera.SetActive(false);
+
+                GameObject nextCamera = Cameras[i];
+
+                nextCamera.SetActive(true);
+            }
+        }
+
+    }
 }
