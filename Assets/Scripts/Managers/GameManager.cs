4ebf0a69281c709f7a530341b70c95a75f7a1e3a
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-07-17 17:55:45 +0300

    New game Manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    public OnlineGameManager OnlineGameManager;
+
+    public delegate void WinCond();
+    public WinCond winCond;
+
+    public delegate void LoseCond();
+    public LoseCond loseCond;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        winCond = InvokeWinCond;
+        loseCond = InvokeLoseCond;
+    }
+    private void InvokeWinCond()
+    {
+        Debug.Log("Win invoked");
+    }
+    private void InvokeLoseCond()
+    {
+        Debug.Log("Lose invoked");
+    }
+}
+
