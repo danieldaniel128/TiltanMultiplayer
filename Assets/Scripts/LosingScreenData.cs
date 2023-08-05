ab67da473ef79c851543967b5b9f3f87524b9b73
amit800IL <108745954+amit800IL@users.noreply.github.com>
2023-08-05 22:06:24 +0300

    endingScreen and json

---
+++ Assets/Scripts/LosingScreenData.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class LosingScreenData
+{
+    public string ConvertToJson(GameData gameData)
+    {
+        string JsonString = JsonUtility.ToJson(gameData);
+        return JsonString;
+    }
+
+    public GameData ReadFromJson(string gameDataStringFromJson)
+    {
+        GameData gamedata = JsonUtility.FromJson<GameData>(gameDataStringFromJson);
+        return gamedata;
+    }
+
+    [System.Serializable]
+    public class GameData
+    {
+        public int alienNumber;
+        public int escapersNumber;
+        public CharchterChoosing team;
+        public string objective;
+        public bool succeded;
+        public float gameTimeMinutes;
+        public float gameTimeSeconds;
+    }
+
+}
